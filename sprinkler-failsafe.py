f7ec7b1d00e172a8ec7e345506b1e68ed4af2a13
Dave Cooper <dave@spudooli.com>
2017-11-21 16:49:58 +1300

    Add log file checking to see if zone running

--- sprinkler-failsafe.py
+++ sprinkler-failsafe.py
@@ -1,5 +1,5 @@
 #Run from cron every minute. Checks if any zones are on and if so allows them to run for no 
-# more than 90 minutes before shutting them off automatically
+# more than 60 minutes before shutting them off automatically
 #Is a fail safe to ensure that even if the Pi loses WIFI, the water won't run all night.
 
 from relay_lib_seeed import *
@@ -11,19 +11,24 @@ installedZones = int(5)
 
 def checkAllZones():
 	for x in range(1, installedZones):
-            if relay_get_port_status(x):
-                print "yep"
-		#logging.info('Zone' + str(x) + 'on')
+		if relay_get_port_status(x):
+			print "yep"
+			logging.info('Zone' + str(x) + ' on')
 
 def checkLogFile():
-    #something
-    print "check"
+	zonecount = 0
+	for x in range(1, installedZones):	
+		file = open("/tmp/pi-sprinkler.log", "r")
+		for line in file:
+			if "Zone" + str(x) in line:
+				zonecount = zonecount + 1
+		if zonecount > 60:
+				turnOffZone(x)
 
-def turnOffZone():
-    #something
-    print installedZones
+def turnOffZone(zone):
+	#something
+	print "Turning off zone " + zone
 
 checkAllZones()
-
 
 checkLogFile()
