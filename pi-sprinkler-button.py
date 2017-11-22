ff8c5fda962f43f91061e3c1a8498eed41a6f924
Dave Cooper <dave@spudooli.com>
2017-11-22 15:34:56 +1300

    Rethink GPIO handling

--- pi-sprinkler-button.py
+++ pi-sprinkler-button.py
@@ -14,17 +14,26 @@ import time
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+button_list = [17,18] 
+
+GPIO.setup(button_list, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+
+def buttonZone1(channel):  
+    print "Zone 1 button pressed"
+
+def buttonZone2(channel):  
+    print "Zone 2 button pressed"
+
+GPIO.add_event_detect(17, GPIO.FALLING, callback=buttonZone1, bouncetime=300)
+
+GPIO.add_event_detect(18, GPIO.FALLING, callback=buttonZone2, bouncetime=300)
 
 
 def DoTheLoop():
     count = 0
     while count < 30:
-        input_state = GPIO.input(18)
-        if input_state == False:
-            print "Button Pushed"
-            #relay_on(zone)
-            time.sleep(0.2)
+        print count
 
 while 1:
     DoTheLoop()
