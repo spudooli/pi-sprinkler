7b39cf4f69683691fb583c0bda08ea4e8333188f
Dave Cooper <dave@spudooli.com>
2017-11-28 14:02:56 +1300

    Simplified LED code

--- pi-sprinkler-button.py
+++ pi-sprinkler-button.py
@@ -16,36 +16,18 @@ GPIO.setmode(GPIO.BCM)
 
 Led_status = 1
 
-button_list = [17,18] 
-zone1LEDpin = 11
-zone2LEDpin = 12
+button_list = 18
+zone1LEDpin = 23
+
 
 GPIO.setup(button_list, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 GPIO.output(zone1LEDpin, GPIO.HIGH) # Set LedPin high(+3.3V) to off led
-GPIO.output(zone2LEDpin, GPIO.HIGH) # Set LedPin high(+3.3V) to off led
 
-def zone1LED(status):
-    GPIO.output(zone1LEDpin, status)  # switch led status(on-->off; off-->on)
-    if status == 1:
-        print 'led off...'
-    else:
-        print '...led on'
-
-def zone2LED(status):
-    GPIO.output(zone2LEDpin, status)  # switch led status(on-->off; off-->on)
-    if status == 1:
-        print 'led off...'
-    else:
-        print '...led on'
-
-def buttonZone1(channel):  
+def buttonZone1(channel):
     print "Zone 1 button pressed"
+    GPIO.output(zone1LEDpin, status)
 
-def buttonZone2(channel):  
-    print "Zone 2 button pressed"
-
-GPIO.add_event_detect(17, GPIO.FALLING, callback=buttonZone1, bouncetime=300)
 
 GPIO.add_event_detect(18, GPIO.FALLING, callback=buttonZone2, bouncetime=300)
 
