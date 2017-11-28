74b171f504545a6bbabb4a16651478f2321cb40a
Dave Cooper <dave@spudooli.com>
2017-11-28 14:05:55 +1300

    Set LED pin to output

--- pi-sprinkler-button.py
+++ pi-sprinkler-button.py
@@ -21,6 +21,8 @@ zone1LEDpin = 23
 
 
 GPIO.setup(button_list, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(zone1LEDpin,GPIO.OUT)
+
 
 GPIO.output(zone1LEDpin, GPIO.HIGH) # Set LedPin high(+3.3V) to off led
 
